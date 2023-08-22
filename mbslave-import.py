b8e9c15c5cc84a15d31cf942f1c9fd7c7d48805d
Rod Johnson <johnsonroda@gmail.com>
2023-08-23 09:27:57 +1000

    Update mbslave-import.py

--- mbslave-import.py
+++ mbslave-import.py
@@ -7,7 +7,7 @@ from mbslave import Config, connect_db, parse_name, check_table_exists, fqn
 
 
 def load_tar(filename, db, config, ignored_schemas, ignored_tables):
-    print "Importing data from", filename
+    print ("Importing data from", filename)
     tar = tarfile.open(filename, 'r:bz2')
     cursor = db.cursor()
     for member in tar:
@@ -17,20 +17,26 @@ def load_tar(filename, db, config, ignored_schemas, ignored_tables):
         schema, table = parse_name(config, name)
         fulltable = fqn(schema, table)
         if schema in ignored_schemas:
-            print " - Ignoring", name
+            print (f" - Ignoring {name} from schema {schema}: Ignored schemas are {ignored_schemas}")
             continue
         if table in ignored_tables:
-            print " - Ignoring", name
+            print (" - Ignoring", name, " from ", ignored_tables)
             continue
         if not check_table_exists(db, schema, table):
-            print " - Skipping %s (table %s does not exist)" % (name, fulltable)
+            print (" - Skipping %s (table %s does not exist)" % (name, fulltable))
             continue
         cursor.execute("SELECT 1 FROM %s LIMIT 1" % fulltable)
+        print(f"Table {fulltable} exists and could run query against it")
         if cursor.fetchone():
-            print " - Skipping %s (table %s already contains data)" % (name, fulltable)
+            print (" - Skipping %s (table %s already contains data)" % (name, fulltable))
             continue
-        print " - Loading %s to %s" % (name, fulltable)
-        cursor.copy_from(tar.extractfile(member), fulltable)
+        print (" - Loading %s to %s" % (name, fulltable))
+
+        # TODO why won't public work
+        frig_table = fulltable.split('.')[-1]
+        print(frig_table)
+
+        cursor.copy_from(tar.extractfile(member), frig_table) #fulltable)
         db.commit()
 
 
